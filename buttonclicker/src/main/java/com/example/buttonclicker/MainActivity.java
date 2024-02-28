7131780bd1a5b27d534cb366573497a2799282f8
Arklim <61851015+akella44@users.noreply.github.com>
2024-02-28 18:51:54 +0300

    Firs practice complete

---
+++ buttonclicker/src/main/java/com/example/buttonclicker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.buttonclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button whoAmIBtn;
+    private Button itIsNotMeBtn;
+    private CheckBox switchingCheckBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.studentTextView);
+        whoAmIBtn = findViewById(R.id.whoAmIbtn);
+        itIsNotMeBtn = findViewById(R.id.itIsNotMeBtn);
+        switchingCheckBox = findViewById(R.id.switchingCheckBox);
+        View.OnClickListener whoAmIOclBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Мой номер по списку 22 или 21 xD (Климин АВ)");
+            }
+        };
+        whoAmIBtn.setOnClickListener(whoAmIOclBtn);
+
+    }
+
+    public void onItIsNotMeBtnClick(View view){
+        textView.setText("Это не я сделал");
+        switchingCheckBox.setChecked(!switchingCheckBox.isChecked());
+    }
+}
--- control_lesson1/src/main/java/com/example/control_lesson1/MainActivity.java
+++ control_lesson1/src/main/java/com/example/control_lesson1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.control_lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       CheckBox rememberCredsCheckBox = (CheckBox)findViewById(R.id.rememberCredCheckBox);
+       rememberCredsCheckBox.setChecked(true);
     }
 }
